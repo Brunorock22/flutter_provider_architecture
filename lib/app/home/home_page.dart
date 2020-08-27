02c6eb443f7c4e88ff563981db6042576fc4a7a4
Bruno Camargos <brunocamargosn@gmail.com>
2020-08-26 21:43:06 -0300

    creating a model of provider architecture

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,12 +2,20 @@ import 'dart:async';
 
 import 'package:firebase_user_avatar_flutter/app/home/about_page.dart';
 import 'package:firebase_user_avatar_flutter/common_widgets/avatar.dart';
+import 'package:firebase_user_avatar_flutter/models/avatar_reference.dart';
+import 'package:firebase_user_avatar_flutter/services/firebase_auth_service.dart';
+import 'package:firebase_user_avatar_flutter/services/firebase_storage_service.dart';
+import 'package:firebase_user_avatar_flutter/services/firestore_service.dart';
+import 'package:firebase_user_avatar_flutter/services/image_picker_service.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   Future<void> _signOut(BuildContext context) async {
     try {
-      // TODO: Implement
+      final auth = Provider.of<FirebaseAuthService>(context);
+      await auth.signOut();
     } catch (e) {
       print(e);
     }
@@ -24,10 +32,22 @@ class HomePage extends StatelessWidget {
 
   Future<void> _chooseAvatar(BuildContext context) async {
     try {
-      // 1. Get image from picker
-      // 2. Upload to storage
-      // 3. Save url to Firestore
-      // 4. (optional) delete local file as no longer needed
+      //Get image from picker
+      final imagePicker = Provider.of<ImagePickerService>(context);
+      final file = await imagePicker.pickImage(source: ImageSource.gallery);
+      if (file != null) {
+        // 2. Upload to storage
+        final user = Provider.of<User>(context, listen: false);
+        final storage = Provider.of<FirebaseStorageService>(context);
+        final downloadUrl =
+            await storage.uploadAvatar(uid: user.uid, file: file);
+        // 3. Save url to Firestore
+        final database = Provider.of<FirestoreService>(context);
+        await database.setAvatarReference(
+            uid: user.uid, avatarReference: AvatarReference(downloadUrl));
+        // 4. (optional) delete local file as no longer needed
+        await file.delete();
+      }
     } catch (e) {
       print(e);
     }
@@ -68,13 +88,20 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _buildUserInfo({BuildContext context}) {
-    // TODO: Download and show avatar from Firebase storage
-    return Avatar(
-      photoUrl: null,
-      radius: 50,
-      borderColor: Colors.black54,
-      borderWidth: 2.0,
-      onPressed: () => _chooseAvatar(context),
+    final database = Provider.of<FirestoreService>(context);
+    final user = Provider.of<User>(context, listen: false);
+    return StreamBuilder(
+      stream: database.avatarReferenceStream(uid: user.uid),
+      builder: (context, snapshot) {
+        final avatarReference = snapshot.data;
+        return Avatar(
+          photoUrl: avatarReference?.downloadUrl,
+          radius: 50,
+          borderColor: Colors.black54,
+          borderWidth: 2.0,
+          onPressed: () => _chooseAvatar(context),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
-import 'package:firebase_user_avatar_flutter/app/sign_in/sign_in_page.dart';
+import 'package:firebase_user_avatar_flutter/app/auth_widget.dart';
+import 'package:firebase_user_avatar_flutter/services/firebase_storage_service.dart';
+import 'package:firebase_user_avatar_flutter/services/firestore_service.dart';
+import 'package:firebase_user_avatar_flutter/services/image_picker_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'services/firebase_auth_service.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: SignInPage(),
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthService>(
+          create: (_) => FirebaseAuthService(),
+        ),Provider<ImagePickerService>(
+          create: (_) => ImagePickerService(),
+        ),Provider<FirebaseStorageService>(
+          create: (_) => FirebaseStorageService(),
+        ),Provider<FirestoreService>(
+          create: (_) => FirestoreService(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.indigo),
+        home: AuthWidget(),
+      ),
     );
   }
 }
